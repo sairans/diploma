fb64beb232e018a54ce1a46690977a3a3aa14f4d
Sanzhar <sairanovs10@gmail.com>
2025-05-06 10:21:33 +0500

    small changes

--- routes/groundRoutes.js
+++ routes/groundRoutes.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const { protect } = require('../middlewares/auth');
+const { admin } = require('../middlewares/auth');
 const Ground = require('../models/ground');
 
 // GET all grounds
 router.get('/', async (req, res) => {
-  const grounds = await Ground.find();
+  const query = {};
+  if (req.query.city) query.city = req.query.city;
+  if (req.query.sport) query.sport = req.query.sport;
+  const grounds = await Ground.find(query);
   res.json(grounds);
 });
 
@@ -16,13 +20,13 @@ router.post('/', protect, async (req, res) => {
 });
 
 // PUT update ground
-router.put('/:id', protect, async (req, res) => {
+router.put('/:id', protect, admin, async (req, res) => {
   const updated = await Ground.findByIdAndUpdate(req.params.id, req.body, { new: true });
   res.json(updated);
 });
 
 // DELETE ground
-router.delete('/:id', protect, async (req, res) => {
+router.delete('/:id', protect, admin, async (req, res) => {
   await Ground.findByIdAndDelete(req.params.id);
   res.status(204).end();
 });
